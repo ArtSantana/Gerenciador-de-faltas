5ca737e240c0bd1d94f8a6e8d21dcb746ab2447e
Alan Victor Papazoglou Violada <alanpapazoglou@gmail.com>
2020-03-06 10:26:20 -0300

    Updates
    Code cleanup, Router and Link config

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import './App.css';
-import Disciplinas from './Disciplinas';
-import Graphs from './Graphs';
-import Calendary from './Calendary';
+import React from "react";
+import "./App.css";
+import Disciplinas from "./Disciplinas";
+import Graphs from "./Graphs";
+import Calendary from "./Calendary";
+import {
+  BrowserRouter as Router,
+  Switch,
+  useLocation,
+  Link,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar"></nav>
-      <Calendary/>
-      <Graphs/>
+      <Router>
+        <nav className="navbar">
+          
+          <ul className="nav-links">
+          <h3 className="nav-links">Gerenciador Faltas</h3>
+            <Link to="/Calendario">
+              <li>Calendario</li>
+            </Link>
+            <Link to="/Faltas">
+              <li>Faltas</li>
+            </Link>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/Calendario" exact={true} component={Calendary} />
+          <Route path="/Faltas" exact={true} component={Graphs} />
+        </Switch>
+      </Router>
     </div>
   );
 }
