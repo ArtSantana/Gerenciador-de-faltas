5c17cc6caf76136fd19ce1919cd40e7e4b80c81e
artsantana <artsantana1457@hotmail.com>
2020-03-07 19:30:59 -0300

    Inserted some other http methods in subject.js

--- server/routes/subject.js
+++ server/routes/subject.js
@@ -12,14 +12,34 @@ conn.connect((err) => {
   if(err) console.log(err);
 });
 
-router.post('/', (req, res) => {
+router.post('/create', (req, res) => {
   const data = req.body;
-  let query = `INSERT INTO Disciplinas(diaAula, horaAula) 
+  const query = `INSERT INTO Disciplinas(diaAula, horaAula) 
               VALUES (${data.dayWeek}, ${data.creditsHour});`
   conn.query(query, (err) => {
-    //if(err) console.log(err);
+    if(err) res.send('Error');
   })  
-  res.send('Toma no cu rapaz');
+  res.send('OK');
+})
+
+router.delete('/', (req, res) => {
+  const data = req.body;
+  const query = `DELETE FROM Disciplinas WHERE ID=${data.id}`
+
+  conn.query(query, (err) => {
+    if(err) res.send('Error');
+  })
+  res.send('OK')
+})
+
+router.post('/search', (req, res) => {
+  const data = req.body;
+  const query = `SELECT * from Disciplinas WHERE ID_Curso=${data.id_curso}`
+  
+  conn.query(query, (err, result) => {
+    if(err) res.send('Error');
+    res.json(result)
+  })
 })
 
 module.exports = router;
