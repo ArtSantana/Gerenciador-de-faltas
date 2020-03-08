fdc7a35c27783452ebb476b471bc3cc781165913
artsantana <artsantana1457@hotmail.com>
2020-03-07 21:27:04 -0300

    Created the student router file

---
+++ server/routes/student.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB
+})
+
+conn.connect((err) => {if(err) console.log(err)});
+
+module.exports = router;
